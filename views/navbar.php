e78f38aed5d0d689e9506cc14d0c1d5ef8bcd974
Antoine <antoinepascualmail@gmail.com>
2022-03-19 10:41:34 +0100

    First commit

--- views/navbar.php
+++ views/navbar.php
@@ -1,8 +1,16 @@
 <script src="https://kit.fontawesome.com/2fc09f2ce5.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/7ab095b2cf.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/highlight.min.js"></script>
 
+<script src="../scripts/highlight.js"></script>
+<script>hljs.highlightAll();</script>
 
 <nav class="navbar navbar-inverse navbar-expand-lg" role="navigation">
-  <a class="navbar-brand" href="home">Site Web</a>
+  <a class="navbar-brand" href="home">TestRouter</a>
   <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-01"></button>
   <div class="collapse navbar-collapse" id="navbar-collapse-01">
     <ul class="nav navbar-nav mr-auto">
